9b63beffbac82ab65fb70975fb0ef6bcbeb7e049
ShawnReardon <shawnreardon1@gmail.com>
2021-05-06 17:40:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    brave.setBounceOnWall(true)
+    otherSprite.setVelocity(sprite.vx * -2, sprite.vy * -2)
+})
+let brave: Sprite = null
 scene.setBackgroundColor(1)
 let zix = sprites.create(assets.image`Zix`, SpriteKind.Player)
 animation.runImageAnimation(
@@ -6,48 +11,9 @@ assets.animation`myAnim`,
 200,
 true
 )
-game.setDialogTextColor(1)
-game.setDialogFrame(img`
-    ...7777............7777..
-    ...77777777777777777777..
-    aaa77777777777777777777aa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    7aaaaaaaaaaaaaaaaaaaaaaaa
-    77aaaaaaaaaaaaaaaaaaaaaa7
-    77a77777777777777777777a7
-    aaa77777777777777777777aa
-    ...7777............7777..
-    `)
-game.showLongText("AHHHHHHHHHHHHHHH", DialogLayout.Full)
-game.showLongText("Who are WE?", DialogLayout.Bottom)
-game.showLongText("WE are ZIX", DialogLayout.Top)
-game.showLongText("I want to DESTROY", DialogLayout.Top)
-game.showLongText("Can't we just...idk...not?", DialogLayout.Bottom)
-game.showLongText("How can WE be the same person?", DialogLayout.Top)
-game.showLongText("I kind of like it. We have balance. ", DialogLayout.Bottom)
-game.showLongText("I suppose you would be weak without me.", DialogLayout.Top)
-game.showLongText("Not...what I meant. But sure. Something like that.", DialogLayout.Bottom)
-let brave = sprites.create(assets.image`brave`, SpriteKind.Player)
+brave = sprites.create(assets.image`brave`, SpriteKind.Player)
+controller.moveSprite(zix)
+brave.follow(zix)
+game.onUpdate(function () {
+	
+})
